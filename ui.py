24ef1b8ff31429317f38fd96d9e4048aa5f3eb8d
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-03-18 00:17:56 +0530

    Connected ui with tesseract

--- ui.py
+++ ui.py
@@ -1,41 +1,46 @@
-import re
 import tkinter as tk
 from datetime import date
 from tkinter import *
-from tkinter import ttk, filedialog, messagebox
+from tkinter import filedialog, messagebox
 from tkcalendar import *
 from PIL import ImageTk,Image
 from tkvideo import tkvideo
+
 filepath=''
 f=1
 def firstui():
-    # Created a Window for Adding Elements
     root = tk.Tk()
+    root.geometry('700x500+500+200')
     root.iconbitmap('images/icon.ico')
+    width = 700
+    height = 500
+
+    root.resizable(False, False)
     root.title('IMEX')
+
 
     lblVideo = Label(root)
     lblVideo.pack()
-
     player = tkvideo("images/test1.mp4",lblVideo,loop=2,size=(700,500))
     player.play()
     lblVideo.after(2000,lblVideo.master.destroy)
-    # Added Software Lookup
 
     root.mainloop()
 def openfile(e):
     global filepath
     global f
+
     if e==0:
-        filepath = filedialog.askopenfilename(filetypes=(("PNG", "*.png"), ("JPG", "*.jpg")))
+        filepath = filedialog.askopenfilename(filetypes=(("JPG", "*.jpg"),("PNG", "*.png")))
         if filepath != '':
             f=0
         if filepath == '':
             messagebox.showerror("Error","You Have Not Uploaded Anything")
-            # messagebox.
         else :
             m = 'The file You Uploaded is ' + filepath
             messagebox.showwarning("File ",m)
+
+
     else :
         return filepath
 def on_submitbuttonClicked(root):
@@ -45,16 +50,21 @@ def on_submitbuttonClicked(root):
             x = openfile(1)
             root.destroy()
             thirdui(x)
-
-
 def Secondui():
     root = tk.Tk()
+    root.geometry('350x550+500+200')
     root.iconbitmap('images/icon.ico')
     root.title('IMEX')
-    root.geometry('350x550')
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    width = 350
+    height = 550
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
     root.config(bg='white')
-
-
+    root.resizable(False, False)
 
     lab2 = Label(root,text='Upload your file Here',font=("Arial",19),bg="white",fg="gray")
     lab2.place(x=45,y=100)
@@ -66,22 +76,21 @@ def Secondui():
     date_entry.place(x=65,y=450)
 
     img = Image.open("images/upload1.jpg")
-    img = img.resize((250,250))  # resize the image to fit the button
-    # Create a PhotoImage object from the image
+    img = img.resize((250,250))
     photo1 = ImageTk.PhotoImage(img)
     upload_button = Button(root,image=photo1,highlightcolor='#111111',borderwidth=0,command=lambda:openfile(0))
-    upload_button.place(x=30,y=150,width=250,height=250)
+    upload_button.place(x=45,y=150,width=250,height=250)
     x = openfile(1)
 
-    img1 = Image.open("images/edit.jpg")
-    img1 = img1.resize((30,30))
-    photo11 = ImageTk.PhotoImage(img1)
-    upload_button1 = Button(root, image=photo11, highlightcolor='#111111', borderwidth=0, command=lambda: openfile(0))
-    upload_button1.place(x=300, y=20, width=30, height=30)
+    edit1 = Image.open("images/edit.jpg")
+    edit1 = edit1.resize((30,30))
+    edit11 = ImageTk.PhotoImage(edit1)
+    edit_button1 = Button(root, image=edit11, highlightcolor='#111111', borderwidth=0)
+    edit_button1.place(x=300, y=20, width=30, height=30)
 
-    lab1 = Label(root,text='Specify The Date:',bg='white',fg='gray')
-    lab1.place(x=65,y=427)
-    #logo
+    datelab1 = Label(root,text='Specify The Date:',bg='white',fg='gray')
+    datelab1.place(x=65,y=427)
+
     image = Image.open("images/logo1.jpg")
     image= image.resize((150,75))
     photo = ImageTk.PhotoImage(image)
@@ -91,27 +100,18 @@ def Secondui():
     img2 = Image.open("images/Adddata.jpg")
     img2 = img2.resize((30, 30))
     photo2 = ImageTk.PhotoImage(img2)
-    upload_button1 = Button(root, img2=photo11, highlightcolor='#111111', borderwidth=0, command=lambda: openfile(0))
-    upload_button1.place(x=300, y=20, width=30, height=30)
-
-    print(x)
-    sub_button = tk.Button(root, text="Submit", bg="#E05959", fg="white",state= 'active' ,font=("Arial", 12),command=lambda:on_submitbuttonClicked(root))
+    upload_button1 = Button(root, image=photo2, highlightcolor='#111111', borderwidth=0)
+    upload_button1.place(x=300, y=60, width=30, height=30)
+    sub_button = tk.Button(root, text="Submit", bg="#E05959", fg="white",font=("Arial", 12),command=lambda:on_submitbuttonClicked(root))
     sub_button.config(borderwidth=2, relief="groove", width=25)
     sub_button.place(x=50, y=485)
 
     root.mainloop()
-
-
-
-
-
-
 def thirdui(x):
     root = Tk()
-    print(x)
-    label = Label(root, text='heloooo')
-    label.place(x=80, y=10)
+    root.geometry('350x550+500+200')
+    root.iconbitmap('images/icon.ico')
+    root.title('IMEX')
+    root.config(bg='white')
+    root.resizable(False, False)
     root.mainloop()
-# firstui()
-Secondui()
-
